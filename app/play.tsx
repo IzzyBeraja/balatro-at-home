022479eb3d278055b6fafa052888b24e6a60f5d4
IzzyBeraja <izzyberaja+github@gmail.com>
2025-02-02 03:04:23 -0500

    Added screen size changes to play route

--- app/play.tsx
+++ app/play.tsx
@@ -1,18 +1,33 @@
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function PlayScreen() {
+  const insets = useSafeAreaInsets();
   const { seed, stake, deck } = useLocalSearchParams<
     "/play",
     { seed?: string; stake?: string; deck?: string }
   >();
 
+  const { width, height } = Dimensions.get("window");
+
   return (
-    <View>
-      <Text>GAME!</Text>
-      <Text>{seed}</Text>
-      <Text>{stake}</Text>
-      <Text>{deck}</Text>
+    <View style={{ width, height, marginLeft: -insets.left }}>
+      <View style={styles.mainContainer}>
+        <Text>GAME!</Text>
+        <Text>{seed}</Text>
+        <Text>{stake}</Text>
+        <Text>{deck}</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "rgb(255, 99, 71)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
